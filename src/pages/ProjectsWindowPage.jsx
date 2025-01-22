e5ece63d3d11aeb56ec6562e64662ffea42877db
Marlie Ford <jive.hive@outlook.com>
2025-01-21 19:33:31 -0600

    dazzling styles arrangement

--- src/pages/ProjectsWindowPage.jsx
+++ src/pages/ProjectsWindowPage.jsx
@@ -1,48 +1,78 @@
-// header, link React/dom/css/footer/
-import { React } from "react";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "../styles/ProjectsPage.css";
 
 const ProjectsWindowPage = () => {
-    return (
-        <div id="project3">
-            <div className="project-item">
-                <a href="https://github.com/Clarky13F/new-project-3" target="_blank" rel="noopener noreferrer">
-                    <img src="../src/images/135402.png" className="clickableImage" alt="Project 3" />
-                </a>
-            </div>
-            <div className="project-item">
-                <a href="https://github.com/3lectricfly69/EeriePapers" target="_blank" rel="noopener noreferrer">
-                    <img src="../src/images/Screenshot109.png" className="clickableImage" alt="Eerie Papers" />
-                </a>
-            </div>
-            <div className="project-item">
-                <a href="https://github.com/RPMcQuigg/Vandy-Project-01" target="_blank" rel="noopener noreferrer">
-                <img src="../src/images/repo-screenshot.png" className="clickableImage" alt="Local List"></img></a>
-            </div>
-        </div>
-    )
-}
+  const [hoveredImage, setHoveredImage] = useState(null);
 
+  const projects = [
+    {
+      id: 1,
+      src: "../src/images/135402.png",
+      alt: "Project 3",
+      text: "New Project 3",
+      link: "https://github.com/Clarky13F/new-project-3",
+    },
+    {
+      id: 2,
+      src: "../src/images/Screenshot109.png",
+      alt: "Eerie Papers",
+      text: "Eerie Papers",
+      link: "https://github.com/3lectricfly69/EeriePapers",
+    },
+    {
+      id: 3,
+      src: "../src/images/repo-screenshot.png",
+      alt: "Local List",
+      text: "Local List",
+      link: "https://github.com/RPMcQuigg/Vandy-Project-01",
+    },
+    // Add more projects here
+  ];
 
+  const handleMouseEnter = (id) => {
+    setHoveredImage(id);
+  };
 
-//   const profile = data?.profile || {};
+  const handleMouseLeave = () => {
+    setHoveredImage(null);
+  };
 
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-//   return (
-//     <div>
-//       <h2 className="card-header">
-//         {profile.name}'s friends have endorsed these skills...
-//       </h2>
+  return (
+    <div className="projectsPage">
+      <Header />
 
-//       {profile.skills?.length > 0 && <SkillsList skills={profile.skills} />}
+      <div className="projectsGrid">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="projectItem"
+            onMouseEnter={() => handleMouseEnter(project.id)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="clickableLink"
+            >
+              <img
+                className="projectImage"
+                src={project.src}
+                alt={project.alt}
+              />
+            </a>
+            {hoveredImage === project.id && (
+              <div className="hoverText">{project.text}</div>
+            )}
+          </div>
+        ))}
+      </div>
 
-//       <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-//         <SkillForm profileId={profile._id} />
-//       </div>
-//     </div>
-//   );
-// };
+      <Footer />
+    </div>
+  );
+};
 
-export default ProjectsWindowPage
+export default ProjectsWindowPage;
