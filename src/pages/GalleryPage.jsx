42dad5491887c9b6719f99a63067d718a85b8881
Marlie Ford <jive.hive@outlook.com>
2025-01-23 20:26:14 -0600

    adjusted file paths of gallery images, refactored file structure of image sources

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -8,26 +8,26 @@ const GalleryPage = () => {
 
   // Array of image paths
   const images = [
-    { id: 1, src: "../src/images/no.1Gi/1.jpg", alt: "Image 1" },
-    { id: 2, src: "../src/images/no.1Gi/eleven.jpg", alt: "Image 2" },
-    { id: 3, src: "../src/images/no.1Gi/9.jpg", alt: "Image 3" },
-    { id: 4, src: "../src/images/no.1Gi/sixteen.jpg", alt: "Image 4" },
-    { id: 5, src: "../src/images/no.1Gi/nineteen.jpg", alt: "Image 5" },
-    { id: 6, src: "../src/images/no.1Gi/twelve.jpg", alt: "Image 6" },
-    { id: 7, src: "../src/images/no.1Gi/spree.jpg", alt: "Image 7"},
-    { id: 8, src: "../src/images/no.1Gi/twenty.jpg", alt: "Image 8" },
-    { id: 9, src: "../src/images/no.1Gi/eighteen.jpg", alt: "Image 9" },
-    { id: 10, src: "../src/images/no.1Gi/ten.jpg", alt: "Image 10" },
-    { id: 11, src: "../src/images/no.1Gi/fourteen.jpg", alt: "Image 11" },
-    { id: 12, src: "../src/images/no.1Gi/fifteen.jpg", alt: "Image 12" },
-    { id: 13, src: "../src/images/no.1Gi/six.jpg", alt: "Image 13" },
-    { id: 14, src: "../src/images/no.1Gi/seventeen.jpg", alt: "Image 14" },
-    { id: 15, src: "../src/images/no.1Gi/globus.jpg", alt: "Image 15" },
-    { id: 16, src: "../src/images/no.1Gi/five.jpg", alt: "Image 16" },
-    { id: 17, src: "../src/images/no.1Gi/four.jpg", alt: "Image 17" },
-    { id: 18, src: "../src/images/no.1Gi/supple.jpg", alt: "Image 18" },
-    { id: 19, src: "../src/images/no.1Gi/two.jpg", alt: "Image  19" },
-    { id: 20, src: "../src/images/no.1Gi/one.jpg", alt: "Image 20" }
+    { id: 1, src: "/images/no.1Gi/1.jpg", alt: "Image 1" },
+    { id: 2, src: "/images/no.1Gi/eleven.jpg", alt: "Image 2" },
+    { id: 3, src: "/images/no.1Gi/9.jpg", alt: "Image 3" },
+    { id: 4, src: "/images/no.1Gi/sixteen.jpg", alt: "Image 4" },
+    { id: 5, src: "/images/no.1Gi/nineteen.jpg", alt: "Image 5" },
+    { id: 6, src: "/images/no.1Gi/twelve.jpg", alt: "Image 6" },
+    { id: 7, src: "/images/no.1Gi/spree.jpg", alt: "Image 7"},
+    { id: 8, src: "/images/no.1Gi/twenty.jpg", alt: "Image 8" },
+    { id: 9, src: "/images/no.1Gi/eighteen.jpg", alt: "Image 9" },
+    { id: 10, src: "/images/no.1Gi/ten.jpg", alt: "Image 10" },
+    { id: 11, src: "/images/no.1Gi/fourteen.jpg", alt: "Image 11" },
+    { id: 12, src: "/images/no.1Gi/fifteen.jpg", alt: "Image 12" },
+    { id: 13, src: "/images/no.1Gi/six.jpg", alt: "Image 13" },
+    { id: 14, src: "/images/no.1Gi/seventeen.jpg", alt: "Image 14" },
+    { id: 15, src: "/images/no.1Gi/globus.jpg", alt: "Image 15" },
+    { id: 16, src: "/images/no.1Gi/five.jpg", alt: "Image 16" },
+    { id: 17, src: "/images/no.1Gi/four.jpg", alt: "Image 17" },
+    { id: 18, src: "/images/no.1Gi/supple.jpg", alt: "Image 18" },
+    { id: 19, src: "/images/no.1Gi/two.jpg", alt: "Image  19" },
+    { id: 20, src: "/images/no.1Gi/one.jpg", alt: "Image 20" }
   ];
 
   // Handle opening the lightbox with the clicked image
